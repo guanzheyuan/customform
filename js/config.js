2a13bac58453902c7565dc8b8861fe82495b23c0
jiangchao <jiang.chao6@ztesoft.com>
2019-09-18 17:26:25 +0800

    经济定制页面

--- js/config.js
+++ js/config.js
@@ -3,10 +3,12 @@ var $JJIP = 'http://10.45.67.181:38080';
 var configJson = {
     loadZWURL:{
         customerFormList:$IP+'/estCustomerform/queryCustomerDirform',
+        customerFormListEconomics:$IP+'/estCustomerform/queryCustomerformDir',
         customer:$IP+'/estCustomerformContent/getNewByCid/',
         saveCustomer:$IP+'/estCustomerformContent/insert',
         insertCustomer:$IP+'/estCustomerform/insert',
-        updateCustomer:$IP+'/estCustomerform/update'
+        updateCustomer:$IP+'/estCustomerform/update',
+        deleteRow:$IP+'/estCustomerform/delete/'
     },
     loadJJURl:{
         loadForm:$JJIP+'/formdesign/getAllCustomForm',
